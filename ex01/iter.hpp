fd41121034efa53bbd820861c6e0eaef7b640616
맹주영 <mjy9088@naver.com>
2023-04-16 00:47:52 +0900

    enhance iter

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -1,28 +1,10 @@
-#ifndef EX00_WHATEVER_HPP_INCLUDED
-#define EX00_WHATEVER_HPP_INCLUDED
+#ifndef EX01_ITER_HPP_INCLUDED
+#define EX01_ITER_HPP_INCLUDED
 
 #include <cstddef>
 
-template <typename T> void iter(T *array, size_t length, void (*func)(T &)) {
-  for (size_t i = 0; i < length; i++)
-    func(array[i]);
-}
-
-template <typename T>
-void iter(const T *array, size_t length, void (*func)(const T &)) {
-  for (size_t i = 0; i < length; i++)
-    func(array[i]);
-}
-
-template <typename T>
-void iter(volatile T *array, size_t length, void (*func)(volatile T &)) {
-  for (size_t i = 0; i < length; i++)
-    func(array[i]);
-}
-
-template <typename T>
-void iter(const volatile T *array, size_t length,
-          void (*func)(const volatile T &)) {
+template <typename TData, typename TFunc>
+void iter(TData *array, size_t length, TFunc func) {
   for (size_t i = 0; i < length; i++)
     func(array[i]);
 }
